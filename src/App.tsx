b60838bc694e6c04f459823e9c1c03f64bb71128
Yevhenns <gifestum@gmail.com>
2023-11-23 13:31:04 +0200

    auth page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./modules/Layout/Layout";
 import { Home } from "./pages/Home/Home";
+import { Auth } from "./pages/Auth/Auth";
 
 const App: FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
       </Route>
     </Routes>
   );

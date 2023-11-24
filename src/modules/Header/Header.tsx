5b50686c47be41e7711750190d08bc79e7f19e62
Yevhenns <gifestum@gmail.com>
2023-11-24 15:41:24 +0200

    tailwind for react

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,5 +1,17 @@
 import { FC } from "react";
+import { Input } from "../../UI/Input/Input";
 
 export const Header: FC = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div>
+        <Input placeholder="Пошук" />
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+      </div>
+    </header>
+  );
 };
